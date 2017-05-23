39789242344961306c790f47c48169bc73b0aefe
Anusha Anu <Anusha Anu>
2017-05-23 18:06:57 +0530

    commit by anu

--- index.js
+++ index.js
@@ -2,8 +2,5 @@ console.log("This is my first code file")
 console.log("give by ganesh")
 console.log("this is from Anusha")
 console.log("this is from Harika")
-
-
-
 console.log("this is from anjani")
 console.log("this is from sai")
