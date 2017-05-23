e172d7cceaf0e993fbad38a7ef2c8f398d9301e8
Varma Bhupatiraju <varmab@gmail.com>
2017-05-23 17:02:24 +0530

    varma changes

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 console.log("This is my first code file")
-<<<<<<< b73c40bd649756e54c4ba6ae51a1116d21094f53
 console.log("give by ganesh")
-=======
 console.log("this is from Anusha")
 console.log("this is from Harika")
->>>>>>> 'Anusha'
